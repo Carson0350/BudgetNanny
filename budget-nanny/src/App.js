106ec90d835ee719d98d475c962353307ac15422
Carson0350 <carson.workflow@gmail.com>
2021-01-28 11:44:38 -0700

    file setup and basic card build

--- budget-nanny/src/App.js
+++ budget-nanny/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react'
 
+import { Grid } from '@material-ui/core'
+import Details from './components/Details/Details';
 
 function App() {
   return (
-    <h1>hello world</h1>
+    <div>
+      <Grid container spaceing={0} alignItems='center' justify='center' styles={{ height: '100vh'}}>
+
+        <Grid item xs={12} sm={4}>
+          <Details/>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Details/>
+        </Grid>
+
+      </Grid>
+    </div>
   );
 }
 
